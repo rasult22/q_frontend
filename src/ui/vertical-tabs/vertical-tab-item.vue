6b80765c891c9d73d6ed6a08357abc35013dd7b3
Itachi-MacOS <warrior.006@inbox.ru>
2022-06-19 02:18:54 +0600

    component: vertical-tab-item

---
+++ src/ui/vertical-tabs/vertical-tab-item.vue
@@ -0,0 +1,44 @@
+<script setup>
+import ForwardArrowIcon from 'src/icons/forward-arrow.vue'
+</script>
+<template>
+  <div class="vertical-tab-item">
+    <div class="vertical-tab-item__left">
+      <slot name="icon">
+      </slot>
+    </div>
+    <div class="vertical-tab-item__center">
+      <slot></slot>
+    </div>
+    <div class="vertical-tab-item__right">
+      <ForwardArrowIcon width="16" height="16" />
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.vertical-tab-item {
+  width: 100%;
+  padding: 16px;
+  box-shadow: 0px 4px 10px 0px #00000012;
+  border-radius: 12px;
+  display: flex;
+  align-items: center;
+  &__left {
+    display: flex;
+    align-items: center;
+  }
+  &__center {
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: 500;
+    height: 100%;
+    line-height: 24px;
+    letter-spacing: 0em;
+    text-align: left;
+    margin-left: 16px;
+    margin-right: auto;
+  }
+  &__right {}
+}
+</style>
