0fbd063d98c6199dd1fc09d782a50ddb04d8120e
Itachi-MacOS <warrior.006@inbox.ru>
2022-06-19 04:00:57 +0600

    Component: Inline-Alert

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,9 @@
 <template>
   <q-page class="row justify-evenly">
     <div class="full-width q-px-md">
+      <Alert>
+        Вы можете редактировать или обновить созданный текст
+      </Alert>
       <VerticalTabItem class="q-mt-md" v-for="(niche, index) in Niches" :key="index">
         <template #icon>
             <component :is="niche.icon" width="24" height="24" />
@@ -13,6 +16,7 @@
 
 <script lang="ts" setup>
 import { ref } from 'vue'
+import Alert from 'src/ui/alerts/inline-alert.vue'
 import DiamondIcon from 'src/icons/diamond.vue'
 import CoctailIcon from 'src/icons/coctail.vue'
 import MassageIcon from 'src/icons/massage.vue'
---
+++ src/ui/alerts/inline-alert.vue
@@ -0,0 +1,33 @@
+<script setup>
+import LampIcon from 'src/icons/lamp.vue'
+</script>
+
+<template>
+  <div class="inline-alert">
+    <div class="inline-alert__left">
+      <LampIcon />
+    </div>
+    <div class="inline-alert__title">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+  .inline-alert {
+    display: flex;
+    padding: 16px;
+    background: #FFCCBC;
+    border-radius: 8px;
+    font-family: Roboto;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 16px;
+    letter-spacing: -0.01em;
+    text-align: left;
+    &__left {}
+    &__title {
+      margin-left: 12px;
+    }
+  }
+</style>
