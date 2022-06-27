c54847f0ff37040d26b8312ed3a6c917b86c1e2a
Itachi-MacOS <warrior.006@inbox.ru>
2022-06-28 05:44:16 +0600

    page: Choose Image

--- src/pages/ChooseImage.vue
+++ src/pages/ChooseImage.vue
@@ -1,8 +1,37 @@
 <script setup>
+import BaseButton from 'src/ui/buttons/base-button.vue'
+import ImageConstructor from 'src/features/image-constructor.vue'
+import { useRouter } from 'vue-router'
+import { useHeader } from 'src/composables/useHeader'
+const router = useRouter()
+const { title, routerBack } = useHeader()
+title.value = ''
+routerBack.value = () => {
+  router.push('/generate-text')
+}
+const onClick = () => {
+  // on click
+  router.push('/process-finishing')
+}
 </script>
 <template>
-  <div></div>
+    <div class="full-width choose-image">
+      <ImageConstructor />
+       <BaseButton @click="onClick" class="choose-image__btn">
+          К предпросмотру
+      </BaseButton>
+    </div>
 </template>
 
-<style>
+<style lang="scss" scoped>
+.choose-image {
+  position: relative;
+  &__btn {
+    display: block;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    z-index: 1;
+  }
+}
 </style>
