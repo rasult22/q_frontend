f3e80131c6450a7551f2532f7d211bb2f700e3f0
Itachi-MacOS <warrior.006@inbox.ru>
2022-06-27 23:38:55 +0600

    page: choose-rubrics

--- src/pages/ChooseRubrics.vue
+++ src/pages/ChooseRubrics.vue
@@ -1,7 +1,39 @@
 <script setup>
+import VerticalTabItem from 'src/ui/vertical-tabs/vertical-tab-item.vue'
+import { useRouter } from 'vue-router'
+import { ref } from 'vue'
+import { useHeader } from 'src/composables/useHeader'
+const router = useRouter()
+const { title, routerBack } = useHeader()
+
+title.value = 'Рубрики'
+routerBack.value = () => {
+  router.push('/choose-contents')
+}
+const onClick = () => {
+  router.push('/generate-text')
+}
+const rubrics = ref([
+  {
+    title: 'Интервью/отзывы/фотоотчеты'
+  },
+  {
+    title: 'Обзоры новинок/товаров/услуг'
+  },
+  {
+    title: 'Инфоповоды/памятные даты'
+  },
+  {
+    title: 'Совместимость товаров/доп товар'
+  }
+])
 </script>
 <template>
-  <div></div>
+  <div class="full-width q-px-md">
+  <VerticalTabItem @click="onClick" class="q-mt-md" v-for="({ title }, index) in rubrics" :key="index">
+    {{ title }}
+  </VerticalTabItem>
+  </div>
 </template>
 
 <style>
